b62d55c616d68296003fd522f32c675bd5fe38a1
Dev HK <devhk@override.local>
2016-07-22 04:11:26 +0800

    update code

--- models/artist.js
+++ models/artist.js
@@ -1,7 +1,7 @@
 /// NOTE: Point to ModelBinder path
 import { ModelBinder } from './e-commerce/mw-model-binder';
 /// NOTE: motor-binder.config of demo
-import { configs } from './e-commerce/demo/motor-binder.config';
+import { configs } from './e-commerce/demo/model-binder.config';
 
 module.exports = (Artist) => {
   Artist.on('attached', () => {
